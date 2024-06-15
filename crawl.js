53be833bb9c3799261c9a62c5d4d2dea87248c95
Piotr Kaczmarski <piotr.kaczmarski.ext@assetmax.ch>
2024-06-16 00:02:48 +0200

    6. Crawl the page

--- crawl.js
+++ crawl.js
@@ -28,7 +28,24 @@ function getUrlsFromHtml(htmlBody, baseUrl) {
 
 }
 
-function main() {
+async function crawlPage(currentUrl) {
+    const res = await fetch(currentUrl);
+    if (!res.ok) {
+        console.error(res.body);
+        return;
+    }
+
+    if (!res.headers.get('Content-Type').startsWith('text/html')) {
+        console.error(`Wrong content type: ${res.headers.get('Content-Type')}`);
+        return;
+    }
+
+    const bodyText = await res.text();
+
+    console.log(bodyText);
+}
+
+async function main() {
     const actArgs = process.argv.splice(2);
     if (actArgs.length < 1) {
         console.error("Pass a base url");
@@ -38,8 +55,10 @@ function main() {
         console.error("Pass only ONE url you moron... Jesus Christ...");
         return;
     }
-    console.log(`Starting the crawl: ${actArgs[0]}`);
-    
+    const page = actArgs[0];
+    console.log(`Starting the crawl: ${page}`);
+
+    await crawlPage(page);
 }
 
 main();
