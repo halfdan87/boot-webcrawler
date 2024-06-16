3cb7870b2b1c818b6101d98908f32926aba6aab5
Piotr Kaczmarski <piotr.kaczmarski.ext@assetmax.ch>
2024-06-16 02:47:26 +0200

    7. Adding recursion

--- crawl.js
+++ crawl.js
@@ -2,7 +2,7 @@ import { JSDOM } from 'jsdom';
 
 function normalizeUrl(url) {
     const parsed = new URL(url);
-    if (parsed.pathname === '/') {
+    if (!parsed.pathname || parsed.pathname === '/') {
         return parsed.host;
     }
     return parsed.host + parsed.pathname;
@@ -18,17 +18,52 @@ function getUrlsFromHtml(htmlBody, baseUrl) {
         try {
             url = new URL(a.getAttribute('href'));
         } catch (e) {
-            url = new URL(baseUrl + a.getAttribute('href'));
+            url = new URL(baseUrl.protocol + "//" + baseUrl.hostname + a.getAttribute('href'));
         }
-        return url.protocol + "//" + url.hostname + url.pathname
-
+        return url.toString();
     });
 
     return hrefs;
 
 }
 
-async function crawlPage(currentUrl) {
+async function crawlPage(baseUrl, currentUrl = baseUrl, pages = {}) {
+    console.log('uuuu', currentUrl);
+    if (baseUrl.hostname !== currentUrl.hostname) {
+        console.log(`1111 [${baseUrl.hostname}] != [${currentUrl.hostname}]`)
+        return pages;
+    }
+
+    const normalized = normalizeUrl(currentUrl);
+    console.log('normalized', normalized);
+    if (pages[normalized]) {
+        pages[normalized]++;
+        return pages;
+    } else {
+        pages[normalized] = 1;
+    }
+
+
+    const body = await fetchUrl(currentUrl);
+
+    const urls = getUrlsFromHtml(body, baseUrl);
+    console.log(urls);
+
+    for (let url of urls) {
+
+        console.log(`Try : ${url}`);
+        const newPages = await crawlPage(baseUrl, new URL(url), pages);
+        console.log('new', newPages);
+        for (let key in newPages) {
+            console.log('aaaa', key);
+            pages[key] = newPages[key];
+        }
+    }
+
+    return pages;
+}
+
+async function fetchUrl(currentUrl) {
     const res = await fetch(currentUrl);
     if (!res.ok) {
         console.error(res.body);
@@ -41,8 +76,7 @@ async function crawlPage(currentUrl) {
     }
 
     const bodyText = await res.text();
-
-    console.log(bodyText);
+    return bodyText;
 }
 
 async function main() {
@@ -58,7 +92,9 @@ async function main() {
     const page = actArgs[0];
     console.log(`Starting the crawl: ${page}`);
 
-    await crawlPage(page);
+    const pages = await crawlPage(new URL(page));
+
+    console.log(pages);
 }
 
 main();
